6cf858edb8afbb8c3cfd4b6c85ddfc96fed8f92c
Ivan Trifonov <thereason12d@gmail.com>
2021-10-03 12:04:07 +0300

    fixed the navbar so it has links instead of heading and added default background color to main screen

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const colorText = document.querySelector('#color-value');
 const mainScreen = document.querySelector('.main-screen');
 const colors = ['#BD811A', '#7890A5', '#24E3D1', '#CD67DC', '#A5AA88'];
 
+colorText.innerText = colors[0];
+mainScreen.style.background = colors[0];
+
 button.addEventListener('click', function () {
   let randomColor = colors[Math.floor(Math.random() * 5)];
   colorText.innerText = randomColor;
